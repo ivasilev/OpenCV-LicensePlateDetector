cb67d4235d9ebb2ebfc86a5cfe9f826e4ca8ebe5
Ivan Vasilev <ivan dot v dot vasilev at gmail.com>
2013-10-17 10:59:05 +0300

    Add LBP cascade with 200 positive 1500 negative images

---
+++ out_16_10_2013-9_33_19/train.cmd
@@ -0,0 +1,2 @@
+/home/ivan/OpenCV/installdir/bin/opencv_traincascade -data cascade_p220n1700 -vec pos.vec -bg bg.txt -w 96 -h 32 -precalcValBufSize 1536 -precalcIdxBufSize 1536 -numPos 220 -numNeg 1700
+/home/ivan/OpenCV/installdir/bin/opencv_traincascade -data cascade_p200n1500_lbp -vec pos.vec -bg bg.txt -w 96 -h 32 -precalcValBufSize 1536 -precalcIdxBufSize 1536 -numPos 200 -numNeg 1500 -featureType LBP
